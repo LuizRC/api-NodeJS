76845ab9ce8e2dad45e38a19b23e916918242171
LuizRC <rafael84treex@protonmail.com>
2021-11-24 10:36:54 -0300

    refactoring, create repositories and migration search

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
 
 
 class UserController {
@@ -8,7 +8,7 @@ class UserController {
     async create(request: Request, response: Response) {
         const { name, email } = request.body;
 
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UsersRepository);
 
         const usersAlreadyExists = await usersRepository.findOne({
           email
@@ -34,4 +34,4 @@ class UserController {
 
 }
 
-export {UserController}
+export { UserController };
