7a8d9628304761e732470c0c542868db61953a0b
LuizRC <rafael84treex@protonmail.com>
2021-11-25 15:50:47 -0300

    ajustes no test

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -1,9 +1,30 @@
 import  request  from "supertest";
 import { app } from "../app";
 
+import createConnection from '../database';
+
 describe("Users", () => {
-    request(app).post("/users").send({
-        email: "user@exemple.com",
-        name: "User Example"
+    beforeAll( async () => {
+        const Connection = await createConnection();
+        await Connection.runMigrations();
+    });
+
+    it("Should be able to create a new user", async () => {
+        const response = await request(app).post("/users").send({
+            email: "user@exemple.com",
+            name: "User Example",
+        });
+
+        expect(response.status).toBe(201);
+    });
+
+    it("Should not be able to create a user with exists email", async () => {
+        const response = await request(app).post("/users").send({
+            email: "user@exemple.com",
+            name: "User Example",
+        });
+
+        expect(response.status).toBe(400);
+
     })
-})
+});
